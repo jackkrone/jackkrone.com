69f09e5434486a3037ce9863218791dd3e792826
jackkrone <jackkrone@protonmail.com>
2021-10-17 22:13:56 -0700

    Add TS to project + configure

---
+++ dist/index.jsx
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const Home = () => {
+    return <h1>Hello World!</h1>;
+};
+exports.default = Home;
