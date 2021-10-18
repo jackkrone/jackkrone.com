1d7ab919fe1f8dc9c341726804dabda3d9d453cd
jackkrone <jackkrone@protonmail.com>
2021-10-17 22:51:31 -0700

    Set siteMetaData defaults/placeholders

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  /* About 1/5 down you can see instructions for setting up siteMetaData: https://blog.logrocket.com/set-up-a-typescript-gatsby-app/ */
+  siteMetadata: {
+    title: `Jack Krone`,
+    description: `This site hosts my personal blog and project portfolio.`,
+    author: `Jack Krone`,
+    siteUrl: `https://jackkrone.com`
+  },
   plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-react-helmet'],
 };
