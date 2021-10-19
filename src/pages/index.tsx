77a611cf1785f755476c453b1f28fb2d1ecf90ba
jackkrone <jackkrone@protonmail.com>
2021-10-19 14:20:10 -0700

    Update home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
 const Home = () => {
@@ -6,28 +7,50 @@ const Home = () => {
     <Layout>
       <h1>Hello World</h1>
 
-      <p>
-        I'm Jack. I created this site mainly to learn, but also to share some
-        thoughts that are too long for a twitter thread.
+      <p className="pt-2">
+        I'm Jack. I created this site mainly as an opportunity to learn, but
+        also as a place to share some projects and thoughts.
       </p>
       <p>
-        First, a few notes on how I built this site: I used TypeScript and
-        Gatsby. You might ask, "Why use TypeScript for such a simple site?"
-        Well, because I wanted to learn how TypeScript works with React. I used
-        Bootstrap 5 for styling, and I specifically chose not to use
-        react-bootstrap for this project as Bootstrap 5 no longer uses jQuery
-        anyways, which makes react-bootstrap partially redundant. Plus, I
-        already had experience using true React style components with Material
-        UI, so I used this project to learn authentic Bootstrap.
+        First, a few notes on how I built this site. I used TypeScript and
+        Gatsby, and I deployed it with Netlify. You might ask, "Why use
+        TypeScript for such a simple site?" Well, because I wanted to learn how
+        TypeScript works with React. I used Bootstrap 5 for styling, and I chose
+        not to use react-bootstrap. Since Bootstrap 5 no longer uses jQuery,
+        react-bootstrap is partially redundant. I already had experience using
+        React style components anyways (using Material UI), so I used this
+        project to learn how the authentic form of Bootstrap works.
       </p>
       <p>
-        For an extra challenge I started this project from zero. Literally, with
-        `npm init y`. I had a heck of a time with all the configuration and
-        adding packages one by one, but I also probably learned more from the
-        setup alone than from the process of building the site. I also learned
-        how to use a number of useful tools while getting everything set up,
-        including nvm and react-helmet. I also learned how to get ESLint and
-        Prettier to play nice together with the help of eslint-plugin-prettier.
+        For an extra challenge I started this project from zero. Literally, with{' '}
+        <code
+          style={{
+            background: 'lightgrey',
+            borderRadius: '4px',
+            paddingLeft: '0.5em',
+            paddingRight: '0.5em',
+          }}
+        >
+          npm init -y
+        </code>
+        . I had a heck of a time with all the configuration and adding packages
+        one by one. I probably learned more from the setup alone than from the
+        remainder of the process. I learned how to use a number of useful tools
+        while getting everything set up, including nvm and react-helmet. I also
+        learned how to get ESLint and Prettier to play nice together with the
+        help of eslint-plugin-prettier.
+      </p>
+      <hr />
+      <p>
+        The inspiration for this site comes from{' '}
+        <Link to="https://training.kalzumeus.com/newsletters/archive/do-not-end-the-week-with-nothing">
+          this
+        </Link>{' '}
+        essay by Patrick McKenzie. Over time, I plan to configure my site to{' '}
+        <Link to="https://www.gatsbyjs.com/docs/how-to/routing/adding-markdown-pages/">
+          read markdown
+        </Link>{' '}
+        so adding new content will be minimally intensive.
       </p>
     </Layout>
   );
