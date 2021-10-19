89105b544fbcd9be22ac5b3fca0c69fe4c794700
jackkrone <jackkrone@protonmail.com>
2021-10-19 12:05:44 -0700

    Include config and browser file to prevent annoying eslint error
    This is what the error looked like: https://stackoverflow.com/questions/66642899/how-fix-parsing-error-parseroptions-project-has-been-set-for-typescript-esl

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     title: `Jack Krone`,
     description: `This site hosts my personal blog and project portfolio.`,
     author: `Jack Krone`,
-    siteUrl: `https://jackkrone.com`
+    siteUrl: `https://jackkrone.com`,
   },
   plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-react-helmet'],
 };
