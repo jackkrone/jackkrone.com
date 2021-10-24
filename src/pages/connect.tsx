92e7e09f04f2fc37eac18c24a63c541d2df6e794
jackkrone <jackkrone@protonmail.com>
2021-10-24 16:34:24 -0700

    Update connect page layout

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -6,34 +6,46 @@ const Connect = () => {
   return (
     <Layout>
       <h1>Connect</h1>
-      <br />
-      <hr />
-      <div className="h1 d-flex flex-row justify-content-center justify-content-md-start">
-        <a
-          href="https://github.com/jackkrone"
-          target="_blank"
-          className="px-3 ps-md-0 pe-md-4 me-lg-2"
-          rel="noreferrer"
-        >
-          <i className="bi bi-github text-secondary" />
+      <div className="mt-2 d-flex flex-row align-items-center">
+        <a href="https://github.com/jackkrone" target="_blank" rel="noreferrer">
+          <i className="h5 bi bi-github me-1 link-secondary" />
         </a>
-        <a className="px-3 px-md-4 mx-lg-2" href="mailto:connect@jackkrone.com">
-          <i className="bi bi-envelope-fill text-secondary" />
+        <span className="mx-1">GitHub:</span>
+        <a href="https://github.com/jackkrone" target="_blank" rel="noreferrer">
+          @jackkrone
         </a>
-        <Link
-          to="needToAddTwitterHandle"
-          target="_blank"
-          className="px-3 px-md-4 mx-lg-2"
-        >
-          <i className="bi bi-twitter text-secondary" />
-        </Link>
+      </div>
+      <div className="d-flex flex-row align-items-center">
+        <a href="mailto:connect@jackkrone.com">
+          <i className="h5 bi bi-envelope-fill me-1 link-secondary" />
+        </a>
+        <span className="mx-1">Email:</span>
+        <a href="mailto:connect@jackkrone.com">connect@jackkrone.com</a>
+      </div>
+      <div className="d-flex flex-row align-items-center">
+        <a href="twitter" target="_blank" rel="noreferrer">
+          <i className="h5 bi bi-twitter me-1 link-secondary" />
+        </a>
+        <span className="mx-1">Twitter:</span>
+        <a href="twitter" target="_blank" rel="noreferrer">
+          @twitter
+        </a>
+      </div>
+      <div className="d-flex flex-row align-items-center">
         <a
           href="https://linkedin.com/in/jackkrone"
           target="_blank"
-          className="px-3 px-md-4 mx-lg-2"
           rel="noreferrer"
         >
-          <i className="bi bi-linkedin text-secondary" />
+          <i className="h5 bi bi-linkedin me-1 link-secondary" />
+        </a>
+        <span className="mx-1">LinkedIn</span>
+        <a
+          href="https://linkedin.com/in/jackkrone"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @jackkrone
         </a>
       </div>
     </Layout>
