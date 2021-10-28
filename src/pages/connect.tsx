97d65c90b07b3db5ec5246fa6156f00f4bee79f5
jackkrone <jackkrone@protonmail.com>
2021-10-28 16:12:43 -0700

    simplify classNames and spacing (use margin)

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -5,65 +5,71 @@ import Layout from '../components/Layout';
 const Connect = () => {
   return (
     <Layout title="Connect">
-      <div className="m-a d-flex flex-row align-items-center">
-        <a
-          href="https://github.com/jackkrone"
-          target="_blank"
-          rel="noreferrer"
-          className="h4 me-1 pt-1 link-secondary"
-        >
-          <FaGithub />
-        </a>
-        <span className="mx-1">GitHub:</span>
-        <a href="https://github.com/jackkrone" target="_blank" rel="noreferrer">
-          @jackkrone
-        </a>
-      </div>
-      <div className="d-flex flex-row align-items-center">
-        <a
-          href="mailto:connect@jackkrone.com"
-          className="h4 me-1 pt-1 link-secondary"
-        >
-          <FaEnvelope />
-        </a>
-        <span className="mx-1">Email:</span>
-        <a href="mailto:connect@jackkrone.com">connect@jackkrone.com</a>
-      </div>
-      <div className="d-flex flex-row align-items-center">
-        <a
-          href="https://twitter.com/protajack"
-          target="_blank"
-          rel="noreferrer"
-          className="h4 me-1 pt-1 link-secondary"
-        >
-          <FaTwitter />
-        </a>
-        <span className="mx-1">Twitter:</span>
-        <a
-          href="https://twitter.com/protajack"
-          target="_blank"
-          rel="noreferrer"
-        >
-          @protajack
-        </a>
-      </div>
-      <div className="d-flex flex-row align-items-center">
-        <a
-          href="https://linkedin.com/in/jackkrone"
-          target="_blank"
-          rel="noreferrer"
-          className="h4 me-1 pt-1 link-secondary"
-        >
-          <FaLinkedinIn />
-        </a>
-        <span className="mx-1">LinkedIn:</span>
-        <a
-          href="https://linkedin.com/in/jackkrone"
-          target="_blank"
-          rel="noreferrer"
-        >
-          @jackkrone
-        </a>
+      <div className="d-grid gap-2">
+        <div>
+          <a
+            href="https://github.com/jackkrone"
+            target="_blank"
+            rel="noreferrer"
+            className="fs-4 me-1 link-secondary"
+          >
+            <FaGithub />
+          </a>
+          <span className="mx-1">GitHub:</span>
+          <a
+            href="https://github.com/jackkrone"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @jackkrone
+          </a>
+        </div>
+        <div>
+          <a
+            href="mailto:connect@jackkrone.com"
+            className="fs-4 me-1 link-secondary"
+          >
+            <FaEnvelope />
+          </a>
+          <span className="mx-1">Email:</span>
+          <a href="mailto:connect@jackkrone.com">connect@jackkrone.com</a>
+        </div>
+        <div>
+          <a
+            href="https://twitter.com/protajack"
+            target="_blank"
+            rel="noreferrer"
+            className="fs-4 me-1 link-secondary"
+          >
+            <FaTwitter />
+          </a>
+          <span className="mx-1">Twitter:</span>
+          <a
+            href="https://twitter.com/protajack"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @protajack
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://linkedin.com/in/jackkrone"
+            target="_blank"
+            rel="noreferrer"
+            className="fs-4 me-1 link-secondary"
+          >
+            <FaLinkedinIn />
+          </a>
+          <span className="mx-1">LinkedIn:</span>
+          <a
+            href="https://linkedin.com/in/jackkrone"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @jackkrone
+          </a>
+        </div>
       </div>
     </Layout>
   );
