1cc9f1553eec1d95198123fe304b3f5a5ebd6ffb
jackkrone <jackkrone@protonmail.com>
2021-10-29 16:22:19 -0700

    fix sorting mechanism to match on all browsers

--- src/components/PrintByYear.tsx
+++ src/components/PrintByYear.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import groupByYear from '../helpers/groupByYear';
+import groupByYear, { listType } from '../functions/groupByYear';
 
-const PrintByYear = ({ list }) => {
-  const yearGroups = groupByYear(list);
+interface PrintByYearProps {
+  list: listType[];
+}
 
-  const output = yearGroups.map(group => {
+const PrintByYear = ({ list }: PrintByYearProps) => {
+  // Get groups by year
+  const groups = groupByYear(list);
+
+  return groups.map(group => (
+    <div key={group.year} className="mb-4">
+      <h5>{group.year}</h5>
+      <hr />
+      <div className="d-grid gap-3">
+        {group.posts?.map(post => (
+          <Link
+            to={`${post.slug}`}
+            key={post.id}
+            className="btn btn-light shadow-sm text-start"
+            style={{ borderRadius: '1.5rem' }}
+          >
+            {post.date}: <u className="link-primary">{post.title}</u>
+          </Link>
+        ))}
+      </div>
+    </div>
+  ));
+};
+
+export default PrintByYear;
+
+// =========== I MIGHT NEED TO SPLIT THIS INTO TWO DIFFERENT COMPONENTS ========== //
+
+/* 
+  const output = years.map(group => {
     const year = group[0];
     const posts = group[1];
     return (
@@ -35,3 +65,4 @@ const PrintByYear = ({ list }) => {
 };
 
 export default PrintByYear;
+*/
