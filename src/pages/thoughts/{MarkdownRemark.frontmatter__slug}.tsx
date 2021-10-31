638974d70f09be62829a0337b4dfa22b19a2005a
jackkrone <jackkrone@protonmail.com>
2021-10-31 15:36:44 -0700

    Create templates directly in thoughts + projects directories

---
+++ src/pages/thoughts/{MarkdownRemark.frontmatter__slug}.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../../components/Layout';
+
+const ThoughtsTemplate = ({ data }) => {
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
+
+  return (
+    <Layout title={frontmatter.title}>
+      <p>
+        <strong>{frontmatter.date}</strong>
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  );
+};
+
+export default ThoughtsTemplate;
+
+export const pageQuery = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`;
