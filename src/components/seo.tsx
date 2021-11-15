ba5b841e941213ffdc9bf6404f52f5cce5708a79
jackkrone <jackkrone@protonmail.com>
2021-11-14 22:57:59 -0800

    Set up SEO at base layer (Layout) of app

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-interface SeoProps {
+export interface SeoProps {
   pathName: string;
   title: string;
   image?: string;
